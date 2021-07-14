15bbc6068a06c1162b70c67300f41c568c067c83
Dave Roberts <KilnDR.FLS@gmail.com>
2021-07-14 15:32:49 -0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,45 @@
+import os
+os.system('clear')
+
+# Print Hello World to screen comment note hashtag to identity comment
+#print ("Hello World!") # This is another comment
+''' 3 single quotation marks allows multiple line comments
+line 2 of comment
+line 3 of comment
+'''
+
+# Variables in python
+
+
+'''full_name = "Dave Roberts" # declare variable and assigned string value
+print(full_name)
+'''
+
+#ata types in python
+#Strings - test (wrapped in quotation marks can be single or double quotes)
+#Numbers 
+#Lists 
+#Tuples   - constant, can't be changed use parenthese instead of bracket
+#Dictionaries   - more complicated list  use curly brackets and they have key and value pair
+#Boolean
+
+'''age = 41
+print(age)
+names = [full_name,age]
+print(names[0]) #print single item
+print(names) #print full list
+'''
+'''quote_test = 'We "Welcome" you'
+print(quote_test)
+'''
+fav_pizza = {
+	"Dave": "Cheese",
+	"Bob": "Anchovy",
+	"Mary": "Pepporoni",
+	#"Bob": "Hawiian"      #note last item in dictionary list is returned!
+}
+
+print(fav_pizza["Bob"])
+
+switch = True
+print(switch)
